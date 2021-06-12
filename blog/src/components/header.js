35f2bfcf917292786d7d7fbe4459aaa795f7dfa0
Yeony99 <ny.yeony.kim@gmail.com>
2021-06-12 17:30:46 +0900

    Add icons and Modify header, layout.js

--- blog/src/components/header.js
+++ blog/src/components/header.js
@@ -2,13 +2,39 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import Logo from '../images/logo.svg'
+import Seo from '../components/seo'
+import Github from '../images/github.svg'
+import Til from '../images/til.svg'
+import Contact from '../images/contact.svg'
+import Works from '../images/works.svg'
+const ListLink = props => (
+  <>
+    <Seo title="Yeony" />
+    <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+      <Link className="list" style={{textDecoration:`none`}} data={props.data} to={props.to}>{props.children}</Link>
+    </li>
+  </>
+)
 
 const Header = () => (
   <header
-    style={{ borderBottom: `0.1rem solid #cfcfcf`
+    style={{
+      borderBottom: `0.1rem solid #249232`, display: `flex`, alignItems: `flex-end`, margin: `1rem 1rem`
     }}
   >
-    <img src={Logo} style={{ width: `10rem`, margin:`1rem auto`, display:'flex' }} />
+    <ListLink to="/"><img src={Logo} style={{ width: `10rem`, marginRight:`1rem`}} /></ListLink>
+    <ListLink to="https://github.com/Yeony99" data="@Yeony99">
+      <img src={Github} alt="Github" style={{ width: `2.3rem` }} />
+    </ListLink>
+    <ListLink to="/TIL" data="Today Yeony Learn">
+      <img src={Til} alt="TIL" style={{ width: `2.5rem` }} />
+    </ListLink>
+    <ListLink to="/contact" data="Contact">
+      <img src={Contact} alt="연락" style={{ width: `2.5rem` }} />
+    </ListLink>
+    <ListLink to="/" data="Works">
+      <img src={Works} alt="작업물" style={{ width: `2.5rem` }} /> 
+    </ListLink>
   </header>
 )
 
@@ -21,3 +47,4 @@ Header.defaultProps = {
 }
 
 export default Header
+
--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import Logo from '../images/logo.svg'
 import { Link } from 'gatsby'
-
 const ListLink = props => (
-<li style={{display: `inline-block`, marginRight:`1rem`}}>
-    <Link style={{textDecoration:`none`}} to={props.to}>{props.children}</Link>
-</li>
-
+  <>
+    <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+      <Link style={{ textDecoration: `none` }} to={props.to}>{props.children}</Link>
+    </li>
+  </>
 )
 
 export default ({children}) => (
@@ -14,12 +13,6 @@ export default ({children}) => (
     padding: `0px 1rem`,
     display: `flex`,
     flexDirection: `column`}}>
-      <ul style={{margin:`0 auto`}}>
-        <ListLink to="/">Home</ListLink>
-        <ListLink to="/about">About</ListLink>
-        <ListLink to="/TIL">TIL</ListLink>
-        <ListLink to="/contact">Contact</ListLink>
-      </ul>
     {children}
   </div>
 )
