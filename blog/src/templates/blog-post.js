29f924a9482521d3921e7733d45a7eb42ab41290
Yeony99 <ny.yeony.kim@gmail.com>
2022-06-23 21:22:09 +0900

    Add avatar icon (link to github)

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -1,37 +1,40 @@
 import React from "react";
 import { graphql } from 'gatsby'
-import Contents from "../components/content";
 import Header from "../components/header"
 import styled from "styled-components";
 import Tag from "../components/tag";
 
 const ImgHeader = styled.div`
-height: 25vh;
+    height: 25vh;
     width: 100%;
     background-color: aliceblue;
 `
 
-export default ({data}) => {
+export default ({ data }) => {
     const meta = data.allMarkdownRemark.edges[0].node
     return (
         <>
-            <Header path={meta.frontmatter.slug} title={meta.frontmatter.title}/>
-            <ImgHeader></ImgHeader>
+            <Header path={meta.frontmatter.slug} title={meta.frontmatter.title} />
+            <ImgHeader>
+                <img className="pg-header-img" src="https://img.freepik.com/free-vector/night-ocean-landscape-full-moon-and-stars-shine_107791-7397.jpg?w=2000"/>
+            </ImgHeader>
             <div className="wrap margin-top-1">
                 <div className="wrap">
                     <h1 className="margin-top-1666">{meta.frontmatter.title}</h1>
                     <div className="flex align-center">
-                        <img style={{height:'auto'}} alt="" width="20" height="20" className="avatar-sm circle margin-right-05" src="https://avatars.githubusercontent.com/u/76241233?v=4" />
+                        <a href="https://github.com/Yeony99" target="_blank" className="margin-right-05">
+                            <img style={{ height: 'auto' }} alt="" width="20" height="20" className="avatar-sm circle margin-bottom-1" src="https://avatars.githubusercontent.com/u/76241233?v=4" />
+                        </a>
                         <h6>Yeony (Nayeon Kim) · {meta.frontmatter.date}</h6>
                     </div>
 
                     <p>{meta.internal.content}</p>
 
-                    <hr className="margin-0"/>
+                    <hr className="margin-0" />
                     <div className="flex">{meta.frontmatter.tags.map(tag => <Tag key={tag} className="tag tag-sm">{tag}</Tag>)}</div>
-                    
+
                 </div>
-                
+
             </div>
             {/* <Contents title="hi" subtitle="sub">
 
