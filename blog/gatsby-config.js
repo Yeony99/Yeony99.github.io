d54747bf2278fb850d1763ba1ed1b52b67827c87
Yeony99 <ny.yeony.kim@gmail.com>
2022-07-08 11:39:53 +0900

    Add sitemap & rss plugin

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -31,8 +31,8 @@ module.exports = {
                 return Object.assign({}, node.frontmatter, {
                   description: node.excerpt,
                   date: node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  url: site.siteMetadata.siteUrl + node.frontmatter.slug,
+                  guid: site.siteMetadata.siteUrl + node.frontmatter.slug,
                   custom_elements: [{ "content:encoded": node.html }],
                 })
               })
@@ -45,12 +45,10 @@ module.exports = {
                   nodes {
                     excerpt
                     html
-                    fields { 
-                      slug 
-                    }
                     frontmatter {
                       title
                       date
+                      slug
                     }
                   }
                 }
