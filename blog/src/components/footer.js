d11903440792f6afb5187e3b184bc4f6afed935e
Yeony99 <ny.yeony.kim@gmail.com>
2022-07-11 14:01:31 +0900

    Update footer to add rss

--- blog/src/components/footer.js
+++ blog/src/components/footer.js
@@ -6,8 +6,13 @@ const Footer = () => {
         <div className="margin-top-1 padding-1 w-100 ">
             <div className="wrap flex align-center justify-content-between padding-1">
                 <div style={{fontSize: 0.9+'rem'}}>© 2022 Nayeon Yeony Kim</div>
-                <div>
-                    <a href="https://github.com/Yeony99" target="_blank"><img className="footer-logo" src={require(`../pages/skills/github.svg`).default} /></a>
+                <div className="flex">
+                  <div className="margin-right-1">
+                    <a href="/rss.xml" target="_blank"><img className="footer-logo" src={require(`../pages/skills/rss.svg`).default} /></a>
+                  </div>
+                  <div>
+                      <a href="https://github.com/Yeony99" target="_blank"><img className="footer-logo" src={require(`../pages/skills/github.svg`).default} /></a>
+                  </div>
                 </div>
             </div>
             
